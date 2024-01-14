0553660ad621a4461b39d3353125cb3f009b3ab7
austinquintana <124468025+austinquintana@users.noreply.github.com>
2024-01-13 21:14:30 -0700

    Update settings.py

--- Achievement_2/2.2/A2_Recipe_App/src/recipe_project/settings.py
+++ Achievement_2/2.2/A2_Recipe_App/src/recipe_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&pn=mq0!=e^igkoa5ss7_g-o%3zw948irl4i+zbd!q4j9(=66&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition

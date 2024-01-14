0d4b36eba30c355676978f3f0d835cf3ac15d452
austinquintana <124468025+austinquintana@users.noreply.github.com>
2024-01-13 17:20:49 -0700

    Exercise 2.6

--- Achievement_2/2.2/A2_Recipe_App/src/recipes/views.py
+++ Achievement_2/2.2/A2_Recipe_App/src/recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.

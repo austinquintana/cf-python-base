1227d0d8c59289198bdcd4bbfbb80393800cdb0c
austinquintana <124468025+austinquintana@users.noreply.github.com>
2024-01-13 21:28:56 -0700

    Update settings.py

--- Achievement_2/2.2/A2_Recipe_App/src/recipe_project/settings.py
+++ Achievement_2/2.2/A2_Recipe_App/src/recipe_project/settings.py
@@ -24,8 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
+DEBUG = False
+ALLOWED_HOSTS = [
+    "127.0.0.1"
+]
 
 
 # Application definition

5766868242930e368396a204382290a997bfe86a
austinquintana <124468025+austinquintana@users.noreply.github.com>
2024-02-05 11:39:53 -0700

    Debugging 2.8 Deployment

--- Achievement_2/2.2/A2_Recipe_App/src/recipe_project/settings.py
+++ Achievement_2/2.2/A2_Recipe_App/src/recipe_project/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    "https://limitless-caverns-25962-915afaa81a19.herokuapp.com/"
+    "limitless-caverns-25962-915afaa81a19.herokuapp.com"
 ]
 
 
